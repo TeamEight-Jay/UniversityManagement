c496d684b58970006146658c1e632eebe4677b4b
kovuriabhishek <abhishek.kovuri@coviam.com>
2019-01-15 21:39:56 +0530

    Merge branch 'master' of /Users/kovuriabhishek/Downloads/UniversityManagement with conflicts.

--- src/main/java/com/TeamEight/UniversityManagement/controller/DepartmentController.java
+++ src/main/java/com/TeamEight/UniversityManagement/controller/DepartmentController.java
@@ -1,4 +1,27 @@
 package com.TeamEight.UniversityManagement.controller;
+import com.TeamEight.UniversityManagement.dto.DepartmentDTO;
+import com.TeamEight.UniversityManagement.entity.Department;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/department")
 public class DepartmentController {
+    @Autowired
+    DepartmentService departmentService;
+
+    @RequestMapping(value ="/add",method = RequestMethod.POST )
+    public String create(@RequestBody DepartmentDTO department) {
+        departmentService.add(department);
+        String s = "created";
+        return s;
+    }
+    @RequestMapping(value = "/select" , method = RequestMethod.GET)
+    public DepartmentDTO select(@RequestParam("id") String id) {
+        Department department = departmentService.select(id);
+        DepartmentDTO departmentdto = new DepartmentDTO();
+        BeanUtils.copyProperties(department, departmentdto);
+        return departmentdto;
+    }
 }
