b2513bf09ff96253bcd09f3767e2ce461a3be498
sirdd2505 <anshu25051997>
2019-01-15 21:07:57 +0530

    Department entity

---
+++ src/main/java/com/TeamEight/UniversityManagement/dto/DepartmentDto.java
@@ -0,0 +1,31 @@
+package com.TeamEight.UniversityManagement.dto;
+
+public class DepartmentDto {
+    private String departmentId;
+    private String departmentName;
+    private String headOfDepartment;
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getHeadOfDepartment() {
+        return headOfDepartment;
+    }
+
+    public void setHeadOfDepartment(String headOfDepartment) {
+        this.headOfDepartment = headOfDepartment;
+    }
+}
