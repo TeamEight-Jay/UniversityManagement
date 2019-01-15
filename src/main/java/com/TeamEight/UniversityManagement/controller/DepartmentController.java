2b3403494f9e44ef246c9f73acaf9dde55d08f41
kovuriabhishek <abhishek.kovuri@coviam.com>
2019-01-15 21:50:15 +0530

    controller

--- src/main/java/com/TeamEight/UniversityManagement/controller/DepartmentController.java
+++ src/main/java/com/TeamEight/UniversityManagement/controller/DepartmentController.java
@@ -1,6 +1,7 @@
 package com.TeamEight.UniversityManagement.controller;
 import com.TeamEight.UniversityManagement.dto.DepartmentDTO;
 import com.TeamEight.UniversityManagement.entity.Department;
+import com.TeamEight.UniversityManagement.services.DepartmentService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -8,20 +9,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/department")
 public class DepartmentController {
-    @Autowired
-    DepartmentService departmentService;
+        @Autowired
+        DepartmentService departmentService;
 
-    @RequestMapping(value ="/add",method = RequestMethod.POST )
-    public String create(@RequestBody DepartmentDTO department) {
-        departmentService.add(department);
-        String s = "created";
-        return s;
+        @PostMapping("/add")
+        public DepartmentDTO add(@RequestBody DepartmentDTO departmentDTO) {
+            Department department = new Department();
+            BeanUtils.copyProperties(departmentDTO, department);
+            departmentService.add(department);
+            return null;
+        }
+
+        @RequestMapping(value = "/select", method = RequestMethod.GET)
+        public DepartmentDTO select(@PathVariable String id) {
+            departmentService.select(id);
+            return null;
+        }
     }
-    @RequestMapping(value = "/select" , method = RequestMethod.GET)
-    public DepartmentDTO select(@RequestParam("id") String id) {
-        Department department = departmentService.select(id);
-        DepartmentDTO departmentdto = new DepartmentDTO();
-        BeanUtils.copyProperties(department, departmentdto);
-        return departmentdto;
-    }
-}
+
