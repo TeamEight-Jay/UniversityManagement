94e9e0839fe309ff38f5af1c310a4d81fb9f6686
sayandbhattacharya <sayand97@gmail.com>
2019-01-15 21:37:11 +0530

    Subject controller

--- src/main/java/com/TeamEight/UniversityManagement/controller/SubjectController.java
+++ src/main/java/com/TeamEight/UniversityManagement/controller/SubjectController.java
@@ -1,17 +1,28 @@
 package com.TeamEight.UniversityManagement.controller;
 
+import com.TeamEight.UniversityManagement.dto.SubjectDTO;
+import com.TeamEight.UniversityManagement.entity.Subject;
 import com.TeamEight.UniversityManagement.services.SubjectService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RequestMapping("/subject")
 @RestController
 public class SubjectController {
 	@Autowired
 	SubjectService subjectService;
-	@RequestMapping("/add")
-	public SubjectDTO add(@RequestBody SubjectDTO subjectDTO){
 
+	@PostMapping("/add")
+	public SubjectDTO add(@RequestBody SubjectDTO subjectDTO) {
+		Subject subject = new Subject();
+		BeanUtils.copyProperties(subjectDTO, subject);
+		subjectService.add(subject);
+		return null;
 	}
-}
+
+	@RequestMapping(value = "/select", method = RequestMethod.GET)
+	public SubjectDTO select(@PathVariable String id) {
+		subjectService.select(id);
+		return null;
+	}
