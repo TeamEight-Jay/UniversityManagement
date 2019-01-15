905e54d84af1a50adf969760231bbe44db1d4371
jayjoshi <jay.joshi@coviam.com>
2019-01-15 21:21:24 +0530

    add student,subject,registration entity

---
+++ src/main/java/com/TeamEight/UniversityManagement/entity/Student.java
@@ -0,0 +1,60 @@
+package com.TeamEight.UniversityManagement.entity;
+
+public class Student {
+    private String studentId;
+    private String studentName;
+    private int yearOfJoining;
+    private String course;
+    private Department department;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getYearOfJoining() {
+        return yearOfJoining;
+    }
+
+    public void setYearOfJoining(int yearOfJoining) {
+        this.yearOfJoining = yearOfJoining;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId='" + studentId + '\'' +
+                ", studentName='" + studentName + '\'' +
+                ", yearOfJoining=" + yearOfJoining +
+                ", course='" + course + '\'' +
+                ", department=" + department +
+                '}';
+    }
+}
---
+++ src/main/java/com/TeamEight/UniversityManagement/entity/Subject.java
@@ -0,0 +1,4 @@
+package com.TeamEight.UniversityManagement.entity;
+
+public class Subject {
+}
