91f809b7e94e220554141b4828072314626a60a0
sayandbhattacharya <sayand97@gmail.com>
2019-01-15 21:28:20 +0530

    Interfaces for Repo

--- src/main/java/com/TeamEight/UniversityManagement/controller/SubjectController.java
+++ src/main/java/com/TeamEight/UniversityManagement/controller/SubjectController.java
@@ -1,4 +1,17 @@
 package com.TeamEight.UniversityManagement.controller;
 
+import com.TeamEight.UniversityManagement.services.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/subject")
+@RestController
 public class SubjectController {
+	@Autowired
+	SubjectService subjectService;
+	@RequestMapping("/add")
+	public SubjectDTO add(@RequestBody SubjectDTO subjectDTO){
+
+	}
 }
