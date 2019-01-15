871705fb7fd43a1a1e0d1cf957c0d9040a610eaa
kovuriabhishek <abhishek.kovuri@coviam.com>
2019-01-15 22:20:22 +0530

    DepartmentController and ProfessorController modified done

--- src/main/java/com/TeamEight/UniversityManagement/controller/DepartmentController.java
+++ src/main/java/com/TeamEight/UniversityManagement/controller/DepartmentController.java
@@ -1,29 +1,29 @@
 package com.TeamEight.UniversityManagement.controller;
 import com.TeamEight.UniversityManagement.dto.DepartmentDTO;
 import com.TeamEight.UniversityManagement.entity.Department;
+import com.TeamEight.UniversityManagement.helper.DTOConvertor;
+import com.TeamEight.UniversityManagement.helper.EntityConvertor;
 import com.TeamEight.UniversityManagement.services.DepartmentService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/department")
 public class DepartmentController {
-        @Autowired
-        DepartmentService departmentService;
+    @Autowired
+    DepartmentService departmentService;
 
-        @PostMapping("/add")
-        public DepartmentDTO add(@RequestBody DepartmentDTO departmentDTO) {
-            Department department = new Department();
-            BeanUtils.copyProperties(departmentDTO, department);
-            departmentService.add(department);
-            return null;
-        }
-
-        @RequestMapping(value = "/select", method = RequestMethod.GET)
-        public DepartmentDTO select(@PathVariable String id) {
-            departmentService.select(id);
-            return null;
-        }
+    @PostMapping("/add")
+    public DepartmentDTO add(@RequestBody DepartmentDTO departmentDTO) {
+        Department department = DTOConvertor.toDepartmentEntity(departmentDTO);
+        Department departmentCreated=departmentService.add(department);
+        return EntityConvertor.toDepartmentDTO(departmentCreated);
     }
 
+    @GetMapping("/select/{departmentId}")
+    public DepartmentDTO select(@PathVariable String departmentId) {
+        Department department = departmentService.select(departmentId);
+        DepartmentDTO departmentDTO = EntityConvertor.toDepartmentDTO(department);
+        return departmentDTO;
+    }
+}
--- src/main/java/com/TeamEight/UniversityManagement/controller/ProfessorController.java
+++ src/main/java/com/TeamEight/UniversityManagement/controller/ProfessorController.java
@@ -1,8 +1,9 @@
 package com.TeamEight.UniversityManagement.controller;
 import com.TeamEight.UniversityManagement.dto.ProfessorDTO;
 import com.TeamEight.UniversityManagement.entity.Professor;
+import com.TeamEight.UniversityManagement.helper.DTOConvertor;
+import com.TeamEight.UniversityManagement.helper.EntityConvertor;
 import com.TeamEight.UniversityManagement.services.ProfessorService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,18 +14,16 @@ public class ProfessorController {
         ProfessorService professorService;
 
         @PostMapping("/add")
-        public ProfessorDTO add(@RequestBody ProfessorDTO professorDTO) {
-            Professor professor = new Professor();
-            BeanUtils.copyProperties(professorDTO, professor);
-            professorService.add(professor);
-            return null;
+        public ProfessorDTO add(@RequestBody ProfessorDTO professorDTO){
+            Professor professor=DTOConvertor.toProfessorEntity(professorDTO);
+            Professor professorCreated=professorService.add(professor);
+            return EntityConvertor.toProfessorDTO(professorCreated);
         }
-
-        @RequestMapping(value = "/select", method = RequestMethod.GET)
-        public ProfessorDTO select(@PathVariable String id) {
-            professorService.select(id);
-            return null;
-        }
+    @GetMapping("/select/{professorId}")
+    public ProfessorDTO select(@PathVariable String professorId) {
+        Professor professor = professorService.select(professorId);
+        ProfessorDTO professorDTO = EntityConvertor.toProfessorDTO(professor);
+        return professorDTO;
+    }
     }
 
-}
