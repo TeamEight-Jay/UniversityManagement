f0bbab41b0ed4dece478a84779f143ea99fd6bb4
Jay Joshi <jayjoshi@Jays-MacBook-Pro.local>
2019-01-15 19:44:37 +0530

    Initial commit

---
+++ src/test/java/com/TeamEight/UniversityManagement/UniversityManagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.TeamEight.UniversityManagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UniversityManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
