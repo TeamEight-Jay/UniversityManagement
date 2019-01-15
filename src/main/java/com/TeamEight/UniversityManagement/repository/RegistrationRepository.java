58ba9ccb4a79111c154129c8547202d8a110a66d
sirdd2505 <anshu25051997>
2019-01-16 00:28:26 +0530

    Queries entered

--- src/main/java/com/TeamEight/UniversityManagement/repository/RegistrationRepository.java
+++ src/main/java/com/TeamEight/UniversityManagement/repository/RegistrationRepository.java
@@ -1,6 +1,27 @@
 package com.TeamEight.UniversityManagement.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 
 public interface RegistrationRepository extends CrudRepository<RegistrationRepository, String> {
+    @Query(value = "SELECT SUM(score) FROM Registration WHERE Registration.studentId=Student.studentId " +
+            "&&" + "Registration.semester= :semester ", nativeQuery = true)
+    int findTotalSemesterScore(@Param("semester") int semester);
+
+    @Query(value = "SELECT COUNT(score) FROM Registration WHERE Registration.studentId=Student.studentId " +
+            "&&" + "Registration.semester= :semester ", nativeQuery = true)
+    int findTotalSemesterSubjectCount(@Param("semester") int semester);
+
+    @Query(value = "SELECT SUM(score) FROM Registration WHERE Registration.professorId=:professorId " +
+            "&&" + "Registration.subjectId= :subjectId ", nativeQuery = true)
+    int findProfessorSubjectScore(@Param("professorId") int professorId, @Param("subjectId") String subjectId);
+
+    @Query(value = "SELECT COUNT(score) FROM Registration WHERE Registration.professorId=:professorId " +
+            "&&" + "Registration.subjectId= :subjectId ", nativeQuery = true)
+    int findProfessorSubjectStudentStrength(@Param("professorId") int professorId, @Param("subjectId") String subjectId);
+
 }
+
+
