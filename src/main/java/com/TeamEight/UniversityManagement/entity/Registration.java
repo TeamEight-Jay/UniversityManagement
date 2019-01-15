e50f1a78ff5f331500e062164cca5c38e19937be
jayjoshi <jay.joshi@coviam.com>
2019-01-15 21:28:24 +0530

    add subject,registration entity

--- src/main/java/com/TeamEight/UniversityManagement/entity/Registration.java
+++ src/main/java/com/TeamEight/UniversityManagement/entity/Registration.java
@@ -1,4 +1,70 @@
 package com.TeamEight.UniversityManagement.entity;
 
 public class Registration {
+    private String registrationId;
+    private Student studentId;
+    private Professor professorId;
+    private Subject subjectId;
+    private int semester;
+    private int score;
+
+    public String getRegistrationId() {
+        return registrationId;
+    }
+
+    public void setRegistrationId(String registrationId) {
+        this.registrationId = registrationId;
+    }
+
+    public Student getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Student studentId) {
+        this.studentId = studentId;
+    }
+
+    public Professor getProfessorId() {
+        return professorId;
+    }
+
+    public void setProfessorId(Professor professorId) {
+        this.professorId = professorId;
+    }
+
+    public Subject getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(Subject subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Registration{" +
+                "registrationId='" + registrationId + '\'' +
+                ", studentId=" + studentId +
+                ", professorId=" + professorId +
+                ", subjectId=" + subjectId +
+                ", semester=" + semester +
+                ", score=" + score +
+                '}';
+    }
 }
--- src/main/java/com/TeamEight/UniversityManagement/entity/Subject.java
+++ src/main/java/com/TeamEight/UniversityManagement/entity/Subject.java
@@ -1,4 +1,8 @@
 package com.TeamEight.UniversityManagement.entity;
 
 public class Subject {
+    private String subjectId;
+    private String subjectName;
+    private int maxScore;
+    private Department department;
 }
