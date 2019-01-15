8ed12faecd4bbd94d419ee0c416d59c88338629b
sayandbhattacharya <sayand97@gmail.com>
2019-01-16 00:14:14 +0530

    Subject controller

---
+++ src/main/java/com/TeamEight/UniversityManagement/services/impl/DepartmentServiceIMPL.java
@@ -0,0 +1,24 @@
+package com.TeamEight.UniversityManagement.services.impl;
+
+import com.TeamEight.UniversityManagement.entity.Department;
+import com.TeamEight.UniversityManagement.repository.DepartmentRepository;
+import com.TeamEight.UniversityManagement.services.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentServiceIMPL implements DepartmentService {
+	@Autowired
+	DepartmentRepository departmentRepository;
+	@Override
+	public Department add(Department department) {
+		Department addDepartment = departmentRepository.save(department);
+		return addDepartment;
+	}
+
+	@Override
+	public Department select(String departmentId) {
+		departmentRepository.findOne(departmentId);
+		return null;
+	}
+}
---
+++ src/main/java/com/TeamEight/UniversityManagement/services/impl/StudentServiceIMPL.java
@@ -0,0 +1,16 @@
+package com.TeamEight.UniversityManagement.services.impl;
+
+import com.TeamEight.UniversityManagement.entity.Student;
+import com.TeamEight.UniversityManagement.services.StudentService;
+
+public class StudentServiceIMPL implements StudentService {
+	@Override
+	public Student add(Student student) {
+		return null;
+	}
+
+	@Override
+	public Student select(String studentId) {
+		return null;
+	}
+}
