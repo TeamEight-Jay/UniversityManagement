07b13e7ddd5260b2405e662de6908f7791bf613b
sirdd2505 <anshu25051997>
2019-01-16 00:24:48 +0530

    Merge branch 'master' of /Users/sirdd2505/Downloads/UniversityManagement with conflicts.

--- src/main/java/com/TeamEight/UniversityManagement/repository/RegistrationRepository.java
+++ src/main/java/com/TeamEight/UniversityManagement/repository/RegistrationRepository.java
@@ -1,7 +1,6 @@
 package com.TeamEight.UniversityManagement.repository;
 
-import com.TeamEight.UniversityManagement.entity.Registration;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RegistrationRepository extends CrudRepository<Registration, String> {
+public interface RegistrationRepository extends CrudRepository<RegistrationRepository, String> {
 }
