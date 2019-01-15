8688330fe907d4f1c6b0683958fe966826c2227f
jayjoshi <jay.joshi@coviam.com>
2019-01-15 21:46:10 +0530

    add student,department convertor

--- src/main/java/com/TeamEight/UniversityManagement/helper/DTOConvertor.java
+++ src/main/java/com/TeamEight/UniversityManagement/helper/DTOConvertor.java
@@ -1,12 +1,40 @@
 package com.TeamEight.UniversityManagement.helper;
 
 import com.TeamEight.UniversityManagement.dto.DepartmentDTO;
+import com.TeamEight.UniversityManagement.dto.StudentDTO;
 import com.TeamEight.UniversityManagement.entity.Department;
+import com.TeamEight.UniversityManagement.entity.Professor;
+import com.TeamEight.UniversityManagement.entity.Student;
+import com.TeamEight.UniversityManagement.services.DepartmentService;
+import com.TeamEight.UniversityManagement.services.ProfessorService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 
 public class DTOConvertor {
 
-    public static Department toDepartmentDTO(DepartmentDTO departmentDTO)
+    @Autowired
+    static DepartmentService departmentService;
+    @Autowired
+    static ProfessorService professorService;
+
+    public static Department toDepartmentEntity(DepartmentDTO departmentDTO)
     {
-        return null;
+        Department department=new Department();
+        BeanUtils.copyProperties(departmentDTO,department);
+        Professor headOfDepartment=professorService.select(departmentDTO.getHeadOfDepartment());
+        department.setHeadOfDepartment(headOfDepartment);
+
+        return department;
     }
+
+    public static Student toStudentEntity(StudentDTO studentDTO)
+    {
+        Student student=new Student();
+        BeanUtils.copyProperties(studentDTO,student);
+        Department department=departmentService.select(studentDTO.getDepartment());
+        student.setDepartment(department);
+        return student;
+    }
+
 }
