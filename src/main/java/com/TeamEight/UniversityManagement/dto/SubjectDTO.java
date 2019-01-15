43cbc40120d8cb697e8e72d802636c8aa3c73f76
sirdd2505 <anshu25051997>
2019-01-15 21:34:27 +0530

    ProfessorDTO,RegistrationDTO,SubjectDTO created and fix Subject entity getter and setter

--- src/main/java/com/TeamEight/UniversityManagement/dto/SubjectDTO.java
+++ src/main/java/com/TeamEight/UniversityManagement/dto/SubjectDTO.java
@@ -1,4 +1,40 @@
 package com.TeamEight.UniversityManagement.dto;
 
 public class SubjectDTO {
+    private String subjectId;
+    private String subjectName;
+    private int maxScore;
+    private String department;
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getMaxScore() {
+        return maxScore;
+    }
+
+    public void setMaxScore(int maxScore) {
+        this.maxScore = maxScore;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
