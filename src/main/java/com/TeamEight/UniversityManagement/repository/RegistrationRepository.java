811f7ce927c996a670e063e2431a5d6b75480b8c
jayjoshi <jay.joshi@coviam.com>
2019-01-16 12:52:32 +0530

    commented queries

--- src/main/java/com/TeamEight/UniversityManagement/repository/RegistrationRepository.java
+++ src/main/java/com/TeamEight/UniversityManagement/repository/RegistrationRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 
 public interface RegistrationRepository extends CrudRepository<Registration, String> {
-    @Query(value = "SELECT SUM(score) FROM Registration WHERE Registration.studentId=Student.studentId " +
+    /*@Query(value = "SELECT SUM(score) FROM Registration WHERE Registration.studentId=Student.studentId " +
             "&&" + "Registration.semester= :semester ", nativeQuery = true)
     int findTotalSemesterScore(@Param("semester") int semester);
 
@@ -22,7 +22,7 @@ public interface RegistrationRepository extends CrudRepository<Registration, Str
     @Query(value = "SELECT COUNT(score) FROM Registration WHERE Registration.professorId=:professorId " +
             "&&" + "Registration.subjectId= :subjectId ", nativeQuery = true)
     int findProfessorSubjectStudentStrength(@Param("professorId") int professorId, @Param("subjectId") String subjectId);
-
+*/
 }
 
 
